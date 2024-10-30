3fcc1062c44a47290437c7efa244e167f8739282
Jeong Min Yang <jeong.yang31@gmail.com>
2024-10-30 12:42:01 +0900

    added svg logo

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart'; // Import flutter_svg
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -8,15 +8,18 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          children: [
-            SvgPicture.asset(
-              'assets/images/looklablogo.svg', // Path to your SVG file
-              height: 30, // Adjust size as needed
-            ),
-            const SizedBox(width: 10), // Spacing between the logo and text
-            const Text('LookLab'),
-          ],
+        title: Center(
+          child: Row(
+            mainAxisSize:
+                MainAxisSize.min, // Ensures the Row takes up minimal space
+            children: [
+              SvgPicture.asset(
+                'lib/assets/images/looklablogo.svg', // Correct path to your SVG file
+                height: 50, // Adjust size as needed
+              ),
+              const SizedBox(width: 10), // Spacing between the logo and text
+            ],
+          ),
         ),
       ),
       body: Center(
